933ade6351a2278b9c37fe706106aba7bb396827
Streamrepo <solanluc5@gmail.com>
2025-07-08 15:36:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ if uploaded_file:
 
                 fig, ax = plt.subplots()
                 fig.patch.set_facecolor('#999999')
-                ax.set_facecolor('#cccccc)
+                ax.set_facecolor('#cccccc')
                 ax.scatter(X, y, label="Benchmark data", alpha=0.6)
                 ax.plot(X, slope * X + intercept, color="orange", label="Regression line")
                 ax.scatter([num_employees], [actual], color="red", s=80, label="Your Company", zorder=5)
